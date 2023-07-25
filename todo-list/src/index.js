3326156cafa3d5c68045dde9392c37e4059b4af6
sp003 <pooja.s@in.pega.com>
2023-07-25 17:10:13 +0530

    Added fontAwesome

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Buttondes } from './Components/Buttondes';
-import { TaskTable } from './Components/TaskTable';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "font-awesome/css/font-awesome.min.css";
+import "./index.css";
+import App from "./App";
+import { Buttondes } from "./Components/Buttondes";
+import { TaskTable } from "./Components/TaskTable";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 //import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-   <App/>
+    <App />
   </React.StrictMode>
 );
 
